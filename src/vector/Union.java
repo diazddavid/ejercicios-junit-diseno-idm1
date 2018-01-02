aa85e63d1ddbeb1f0aca2d47e3092895bc3262f6
diazddavid <diazrivasdavid5@gmail.com>
2018-01-02 11:18:37 +0100

    First Commit

---
+++ src/vector/Union.java
@@ -0,0 +1,17 @@
+package vector;
+
+import java.util.Vector;
+
+public class Union {
+	/**
+	* @return New object (different from a and b) of class Vector that
+	contains the union set (NO elements can be repeated) of a and b
+	* @param a Vector containing elements, including null. It is not a set, so it can have repeated elements, and they can be of different classes / types
+	* @param b Vector containing elements, including null. It is not a set,	so it can have repeated elements, and they can be of different classes / types
+	* @throws NullPointerException If a or b is null
+	*/
+	
+	public static VectorUnion (Vector a, Vector b){
+		
+	}
+}
---
+++ src/vector/UnionTest.java
@@ -0,0 +1,12 @@
+package vector;
+
+import java.util.Vector;
+
+import org.junit.Test;
+
+public class UnionTest {
+	@Test public void UnionTest
+	   {
+	      
+	   }
+}
